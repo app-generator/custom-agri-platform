75f56a5c00af265e8132b0946be165d0b73bd70a
mominur774 <mominurrohman774@gmail.com>
2026-03-18 20:55:10 +0600

    Added xlsx feature

--- apps/pages/templatetags/custom_tags.py
+++ apps/pages/templatetags/custom_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from apps.users.models import UserRole
-from apps.common.models import Role, TabFields
+from apps.common.models import Role, TabFields, Tab
 
 register = template.Library()
 
@@ -29,3 +29,16 @@ def get_cell(cells, field_id):
 @register.filter
 def is_col_exists(tab):
     return TabFields.objects.filter(tab=tab).exists()
+
+
+@register.filter
+def get_first_tab(sheet):
+    tabs = Tab.objects.filter(sheet=sheet)
+    if tabs.exists():
+        return tabs.first().pk
+    else:
+        tab = Tab.objects.create(
+            sheet=sheet,
+            name="Tab 1"
+        )
+        return tab.pk

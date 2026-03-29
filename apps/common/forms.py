1f376da5b618fa0c20e80eb312673e33d0de73f3
mominur774 <mominurrohman774@gmail.com>
2026-03-29 21:49:27 +0600

    Fixed shape file and dark mode issues

--- apps/common/forms.py
+++ apps/common/forms.py
@@ -34,7 +34,7 @@ class SheetForm(forms.ModelForm):
             field.widget.attrs['class'] = (
                 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm '
                 'rounded-lg focus:ring-primary-500 focus:border-primary-500 '
-                'block w-full p-2.5'
+                'block w-full p-2.5 dark:bg-gray-800 dark:text-white'
             )
 
             if field_name == 'info':
